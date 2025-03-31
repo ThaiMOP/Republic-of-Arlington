183e4df18f8f49de88588e59e2fa1cd97ef63aba
ThaiMOP <npgm00000@gmail.com>
2025-03-31 19:26:19 +0700

    Update pe.js

--- assets/js/pe.js
+++ assets/js/pe.js
@@ -63,7 +63,7 @@ function updateClock() {
             
                 // รอ 2.6 วินาทีแล้วเปลี่ยนหน้า
                 setTimeout(function () {
-                    window.location.href = "/election/";
+                    window.location.href = "/election/next";
                 }, 2600);
             })
 
