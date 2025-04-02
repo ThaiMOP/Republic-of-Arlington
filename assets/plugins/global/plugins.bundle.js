ded325b917c08d5272bc0d64f1028d0d22f16454
ThaiMOP <npgm00000@gmail.com>
2025-04-02 11:17:20 +0700

    Update plugins.bundle.js

--- assets/plugins/global/plugins.bundle.js
+++ assets/plugins/global/plugins.bundle.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="d-flex justify-content-center align-items-center h-100">
                 <div class="card rounded-3 w-md-550px shadow">
                     <div class="card-body d-flex flex-column p-10 p-lg-20 pb-lg-10">
+                        <img src="/assets/img/eca-login.png">
                         <h2 style="text-align: center;">ระบบฐานข้อมูลพรรคการเมือง</h2>
                         <label for="email2">อีเมล / ผู้ใช้งาน</label>
                         <input type="text" id="email2" placeholder="กรอกอีเมลหรือชื่อผู้ใช้" style="width: -webkit-fill-available; padding: 10px; margin: 5px 0; border: 1px solid #ccc; border-radius: 5px;">
@@ -17,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
                             <a href="#" style="text-decoration: none; color: blue;">ลืมรหัสผ่าน ?</a>
                         </div>
                         
-                        <button id="loginButton" style="width: -webkit-fill-available; padding: 10px; margin-top: 10px; background-color: blue; color: white; border: none; border-radius: 5px; cursor: pointer;">
+                        <button id="loginButton">
                             เข้าสู่ระบบ
                         </button>
                     </div>
