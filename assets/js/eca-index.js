24455c59579a0e166797c2b9c92347e40b649912
ThaiMOP <npgm00000@gmail.com>
2025-04-07 09:16:32 +0700

    Update eca-index.js

--- assets/js/eca-index.js
+++ assets/js/eca-index.js
@@ -46,7 +46,8 @@ document.addEventListener("DOMContentLoaded", function () {
     </nav>
     
     <div class="banner">
-        <img src="https://rep-arlington.pages.dev/assets/img/eca-banner.png" width="100%" alt="Election Commission Banner">
+        <img src="https://rep-arlington.pages.dev/assets/img/eca-banner.png" width="100%" alt="Election Commission Banner" style="display: none">
+        <img src="https://rep-arlington.pages.dev/assets/img/eca-banner (1).png" width="100%" alt="Election Commission Banner">
     </div>
 
     <div class="container">
