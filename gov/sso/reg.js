a4dd26698eb9ae4822b97d114273063453aaf954
ThaiMOP <npgm00000@gmail.com>
2025-04-11 14:21:11 +0700

    Update reg.js

--- gov/sso/reg.js
+++ gov/sso/reg.js
@@ -1,21 +1,22 @@
 
   const checkbox = document.getElementById("myCheck");
-  const button = document.getElementById("nextBtn");
+  const button1 = document.getElementById("nextBtn1");
+  const button2 = document.getElementById("nextBtn2");
   const page1 = document.getElementById("page-1");
   const page2 = document.getElementById("page-2");
 
   checkbox.addEventListener("change", function () {
     if (this.checked) {
-      button.disabled = false;
-      button.style.backgroundColor = "#029d00";
+      button1.disabled = false;
+      button1.style.backgroundColor = "#029d00";
     } else {
-      button.disabled = true;
-      button.style.backgroundColor = "#1cc61c";
+      button1.disabled = true;
+      button1.style.backgroundColor = "#1cc61c";
     }
   });
 
-  button.addEventListener("click", function () {
-    if (!button.disabled) {
+  button1.addEventListener("click", function () {
+    if (!button1.disabled) {
       page1.classList.add("hidden");
       page2.classList.remove("hidden");
     }
