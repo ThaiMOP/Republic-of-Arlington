661072bbd9a84b1ff30ea6c14cea5bc45df1e374
ThaiMOP <npgm00000@gmail.com>
2025-04-11 12:14:07 +0700

    Create reg.js

---
+++ gov/sso/reg.js
@@ -0,0 +1,22 @@
+
+  const checkbox = document.getElementById("myCheck");
+  const button = document.getElementById("nextBtn");
+  const page1 = document.getElementById("page-1");
+  const page2 = document.getElementById("page-2");
+
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      button.disabled = false;
+      button.style.backgroundColor = "#029d00";
+    } else {
+      button.disabled = true;
+      button.style.backgroundColor = "#1cc61c";
+    }
+  });
+
+  button.addEventListener("click", function () {
+    if (!button.disabled) {
+      page1.style.display = "none";
+      page2.style.display = "block";
+    }
+  });
