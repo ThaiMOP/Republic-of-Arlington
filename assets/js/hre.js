2c8cd4ce6a0c546a049c59ab8d6ae9c9f4e5a32b
ThaiMOP <npgm00000@gmail.com>
2025-05-04 15:58:40 +0700

    Update hre.js

--- assets/js/hre.js
+++ assets/js/hre.js
@@ -4,7 +4,7 @@
 
     const now = new Date();
     const startTime = new Date(2025, 4, 5, 6, 0, 0);  // 5 พ.ค. 2025 06:00
-    const endTime = new Date(2025, 4, 20, 15, 59, 45); // 20 พ.ค. 2025 15:59:45
+    const endTime = new Date(2025, 4, 5, 20, 59, 45); // 5 พ.ค. 2025 20:59:45
 
     let nextState = "";
     if (now < startTime) {
