9d6a6b1f781790a6cbc0bfbdc4fd536dde9d43a7
ThaiMOP <npgm00000@gmail.com>
2025-05-06 14:33:47 +0700

    Update script.js

--- ARLID/assets/script.js
+++ ARLID/assets/script.js
@@ -1,9 +1,8 @@
+const clientId = '1361187603352784967';
+const redirectUri = 'https://rep-arlington.pages.dev/ARLID/callback';
+const scope = 'identify';
 
-    const clientId = '1361187603352784967';
-    const redirectUri = 'https://rep-arlington.pages.dev/ARLID/callback';
-    const scope = 'identify';
-
-    document.getElementById('login-btn').addEventListener('click', () => {
-      const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=${scope}`;
-      window.location.href = url;
-    });
+document.getElementById('login-btn').addEventListener('click', () => {
+  const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=${scope}`;
+  window.location.href = url;
+});
